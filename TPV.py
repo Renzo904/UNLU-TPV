f4c4cb9b433a43ba7b797b7eeffcea5f37dcc477
Renzo Martin Poggio <63516530+Renzo904@users.noreply.github.com>
2023-03-28 10:58:39 -0300

    Update TPV.py

--- TPV.py
+++ TPV.py
@@ -6,6 +6,7 @@ def suma(x, y):
 
 #Problema 2
 def sumaret(x, y):
+    print(x + y)
     return x + y
 
 #Problema 3
@@ -27,3 +28,26 @@ def mayus(string):
 #Problema 7
 def comparacion(nombre1, nombre2):
     return len(nombre1) > len(nombre2)
+
+
+#Problema 9
+def testpotencia():
+    assert(potencia(2, 3) == 8)
+    assert(potencia(3, 2) == 9)
+    assert(potencia(5, 3) == 125)
+
+    print("Paso")
+
+
+#Problema 1
+numerouno = input("Ingrese el primer numero: ")
+numerodos = input("Ingrese el segundo numero: ")
+print("La suma de esos dos numeros es lo siguiente: ")
+suma(numerouno, numerodos)
+
+#Problema 3
+nombre = input("Ingrese su nombre: ")
+print(f"Su nombre tiene {longitudstr(nombre)} letras")
+
+#Problema 9
+testpotencia()
